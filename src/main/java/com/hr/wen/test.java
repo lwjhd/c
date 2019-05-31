a0a94c113ce4b0c6ba124a69233957e27bc439a6
Your Name <you@example.com>
2019-05-31 23:57:59 +0800

    sss

--- src/main/java/com/hr/wen/test.java
+++ src/main/java/com/hr/wen/test.java
@@ -4,9 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 
-		System.out.println("aaadadwadssss");
-
-		System.out.println("aaadadwadss");
+		System.out.println("aaadadwadssssdwadwadawwd");
 
 	}
 
