1ff182b1b279c9a7dae598b3c1609e3379c6544c
Your Name <you@example.com>
2019-06-01 00:27:22 +0800

    ddwww

--- src/main/java/com/hr/wen/test.java
+++ src/main/java/com/hr/wen/test.java
@@ -5,7 +5,7 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("dw1dwddwdw1d1wsss");
-		System.out.println("sss");
+		System.out.println("dw1dwddwdw1d1wsss");
 	}
 
 
